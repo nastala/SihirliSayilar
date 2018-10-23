f616da1055bfb8fd858ac9627bcd2765f0e964b2
nastala <sezginyagli@gmail.com>
2018-10-23 23:51:05 +0300

    Initial commit

---
+++ app/src/main/java/com/nastala/sihirlisayilar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nastala.sihirlisayilar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
